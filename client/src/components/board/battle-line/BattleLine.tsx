d073feb6f8a729991bea752ecdbca957f4385c2e
seyed0123 <seyed123ali123@gmail.com>
2025-09-11 13:56:30 +0330

    change the battle line border and alert

--- client/src/components/board/battle-line/BattleLine.tsx
+++ client/src/components/board/battle-line/BattleLine.tsx
@@ -77,6 +77,9 @@ export const BattleLine = (props: { state: PlayerState; moves: Moves; ctx: GameC
       }}
       data-dragging={isDragging ? "true" : "false"}
     >
+      <div className={`${styles.PlayerIcon} ${isCurrentTurn ? styles.Active : ""}`}>
+      {playerId === "0" ? "👑" : "⚔️"}
+      </div>
       <div className={styles.Particles} aria-hidden="true" />
       <TransitionGroup component={null}>
         {model.map((card: ICardModel) => {
